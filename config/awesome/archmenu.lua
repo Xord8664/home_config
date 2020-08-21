99b5794682a86432f61909bbe295913b2bf99b46
Yushin Dmitry <dymnord@gmail.com>
2020-08-21 22:43:51 +0300

    menu for awesome

---
+++ config/awesome/archmenu.lua
@@ -0,0 +1,143 @@
+ local menu98edb85b00d9527ad5acebe451b3fae6 = {
+     {"Application Finder", "xfce4-appfinder", "/usr/share/icons/gnome/16x16/actions/edit-find.png" },
+     {"Atom", "env ATOM_DISABLE_SHELLING_OUT_FOR_ENVIRONMENT=false /usr/bin/atom ", "/usr/share/icons/hicolor/16x16/apps/atom.png" },
+     {"Disks", "gnome-disks"},
+     {"Double Commander", "doublecmd ", "/usr/share/pixmaps/doublecmd.png" },
+     {"Nemo", "nemo ", "/usr/share/icons/gnome/16x16/places/folder.png" },
+     {"PlayOnLinux", "playonlinux ", "/usr/share/pixmaps/playonlinux.png" },
+     {"Redshift", "redshift-gtk"},
+     {"Remarkable", "/usr/bin/remarkable "},
+     {"Software Token", "stoken-gui", "/usr/share/pixmaps/stoken-gui.png" },
+     {"Software Token (small)", "stoken-gui --small", "/usr/share/pixmaps/stoken-gui.png" },
+     {"Vim", "xterm -e vim ", "/usr/share/icons/hicolor/48x48/apps/gvim.png" },
+     {"Winetricks", "winetricks --gui"},
+     {"Xarchiver", "xarchiver ", "/usr/share/icons/hicolor/16x16/apps/xarchiver.png" },
+     {"nitrogen", "nitrogen", "/usr/share/icons/hicolor/16x16/apps/nitrogen.png" },
+ }
+
+ local menude7a22a0c94aa64ba2449e520aa20c99 = {
+     {"LibreOffice Math", "libreoffice --math ", "/usr/share/icons/hicolor/16x16/apps/libreoffice-math.png" },
+ }
+
+ local menu251bd8143891238ecedc306508e29017 = {
+     {"AstroMenace", "astromenace", "/usr/share/pixmaps/astromenace.png" },
+     {"Fceux", "/usr/bin/fceux", "///usr/share/pixmaps/fceux.png" },
+     {"OpenMW Content Editor", "openmw-cs", "/usr/share/pixmaps/openmw-cs.png" },
+     {"OpenMW Launcher", "openmw-launcher", "/usr/share/pixmaps/openmw.png" },
+     {"Steam (Runtime)", "/usr/bin/steam-runtime ", "/usr/share/icons/hicolor/16x16/apps/steam.png" },
+ }
+
+ local menud334dfcea59127bedfcdbe0a3ee7f494 = {
+     {"Converseen", "converseen", "/usr/share/icons/hicolor/256x256/apps/converseen.png" },
+     {"Flameshot", "flameshot", "/usr/share/icons/hicolor/48x48/apps/flameshot.png" },
+     {"GNU Image Manipulation Program", "gimp-2.10 ", "/usr/share/icons/hicolor/16x16/apps/gimp.png" },
+     {"LibreOffice Draw", "libreoffice --draw ", "/usr/share/icons/hicolor/16x16/apps/libreoffice-draw.png" },
+     {"Viewnior", "viewnior ", "/usr/share/icons/hicolor/16x16/apps/viewnior.png" },
+     {"XDvi", "xdvi "},
+ }
+
+ local menuc8205c7636e728d448c2774e6a4a944b = {
+     {"Avahi SSH Server Browser", "/usr/bin/bssh", "/usr/share/icons/gnome/16x16/devices/network-wired.png" },
+     {"Avahi VNC Server Browser", "/usr/bin/bvnc", "/usr/share/icons/gnome/16x16/devices/network-wired.png" },
+     {"Discord", "/usr/bin/discord", "/usr/share/pixmaps/discord.png" },
+     {"FileZilla", "filezilla", "/usr/share/icons/hicolor/16x16/apps/filezilla.png" },
+     {"Google Chrome", "/usr/bin/google-chrome-stable ", "/usr/share/icons/hicolor/16x16/apps/google-chrome.png" },
+     {"Links", "xlinks -g", "/usr/share/pixmaps/links-48x48.xpm" },
+     {"Remote Viewer", "remote-viewer ", "/usr/share/icons/hicolor/16x16/apps/virt-viewer.png" },
+     {"Steam (Runtime)", "/usr/bin/steam-runtime ", "/usr/share/icons/hicolor/16x16/apps/steam.png" },
+     {"Telegram Desktop", "telegram-desktop -- ", "/usr/share/icons/hicolor/16x16/apps/telegram.png" },
+     {"Transmission", "transmission-gtk ", "/usr/share/pixmaps/transmission.png" },
+     {"Wireshark", "wireshark ", "/usr/share/icons/hicolor/16x16/apps/wireshark.png" },
+     {"Zoom", "/usr/bin/zoom ", "/usr/share/pixmaps/Zoom.png" },
+ }
+
+ local menudf814135652a5a308fea15bff37ea284 = {
+     {"Atril Document Viewer", "atril ", "/usr/share/icons/hicolor/16x16/apps/atril.png" },
+     {"Evolution", "evolution "},
+     {"FBReader", "FBReader ", "/usr/share/pixmaps/FBReader.png" },
+     {"LibreOffice", "libreoffice ", "/usr/share/icons/hicolor/16x16/apps/libreoffice-startcenter.png" },
+     {"LibreOffice Base", "libreoffice --base ", "/usr/share/icons/hicolor/16x16/apps/libreoffice-base.png" },
+     {"LibreOffice Calc", "libreoffice --calc ", "/usr/share/icons/hicolor/16x16/apps/libreoffice-calc.png" },
+     {"LibreOffice Draw", "libreoffice --draw ", "/usr/share/icons/hicolor/16x16/apps/libreoffice-draw.png" },
+     {"LibreOffice Impress", "libreoffice --impress ", "/usr/share/icons/hicolor/16x16/apps/libreoffice-impress.png" },
+     {"LibreOffice Math", "libreoffice --math ", "/usr/share/icons/hicolor/16x16/apps/libreoffice-math.png" },
+     {"LibreOffice Writer", "libreoffice --writer ", "/usr/share/icons/hicolor/16x16/apps/libreoffice-writer.png" },
+ }
+
+ local menue6f43c40ab1c07cd29e4e83e4ef6bf85 = {
+     {"Atom", "env ATOM_DISABLE_SHELLING_OUT_FOR_ENVIRONMENT=false /usr/bin/atom ", "/usr/share/icons/hicolor/16x16/apps/atom.png" },
+     {"Bless Hex Editor", "bless ", "/usr/share/pixmaps/bless-48x48.png" },
+     {"CMake", "cmake-gui ", "/usr/share/icons/hicolor/32x32/apps/CMakeSetup.png" },
+     {"DBeaver Community", "dbeaver", "/usr/share/icons/hicolor/16x16/apps/dbeaver.png" },
+     {"Electron", "electron5 ", "/usr/share/pixmaps/electron5.png" },
+     {"FLUID", "fluid ", "/usr/share/icons/hicolor/32x32/apps/fluid.png" },
+     {"GdaBrowser", "gda-browser-5.0", "/usr/share/pixmaps/gda-browser-5.0.png" },
+     {"Geany", "geany ", "/usr/share/icons/hicolor/16x16/apps/geany.png" },
+     {"Meld", "meld ", "/usr/share/icons/hicolor/16x16/apps/org.gnome.meld.png" },
+     {"Qt Assistant", "assistant", "/usr/share/icons/hicolor/32x32/apps/assistant.png" },
+     {"Qt Designer", "designer ", "/usr/share/icons/hicolor/128x128/apps/QtProject-designer.png" },
+     {"Qt Linguist", "linguist ", "/usr/share/icons/hicolor/16x16/apps/linguist.png" },
+     {"Qt QDbusViewer ", "qdbusviewer", "/usr/share/icons/hicolor/32x32/apps/qdbusviewer.png" },
+     {"Qt4 Assistant ", "assistant-qt4", "/usr/share/icons/hicolor/32x32/apps/assistant-qt4.png" },
+     {"Qt4 Designer", "designer-qt4", "/usr/share/icons/hicolor/128x128/apps/designer-qt4.png" },
+     {"Qt4 Linguist ", "linguist-qt4", "/usr/share/icons/hicolor/16x16/apps/linguist-qt4.png" },
+     {"Qt4 QDbusViewer ", "qdbusviewer-qt4", "/usr/share/icons/hicolor/32x32/apps/qdbusviewer-qt4.png" },
+ }
+
+ local menu52dd1c847264a75f400961bfb4d1c849 = {
+     {"Ardour", "ardour6", "/usr/share/icons/hicolor/16x16/apps/ardour6.png" },
+     {"Audacious", "audacious ", "/usr/share/icons/hicolor/48x48/apps/audacious.png" },
+     {"Audacity", "env UBUNTU_MENUPROXY=0 audacity ", "/usr/share/icons/hicolor/16x16/apps/audacity.png" },
+     {"Calf Plugin Pack for JACK", "calfjackhost", "/usr/share/icons/hicolor/16x16/apps/calf.png" },
+     {"Carla", "carla ", "/usr/share/icons/hicolor/16x16/apps/carla.png" },
+     {"Carla Control", "carla-control ", "/usr/share/icons/hicolor/16x16/apps/carla-control.png" },
+     {"GNOME MPlayer", "gnome-mplayer ", "/usr/share/icons/hicolor/16x16/apps/gnome-mplayer.png" },
+     {"Kdenlive", "kdenlive ", "/usr/share/icons/hicolor/16x16/apps/kdenlive.png" },
+     {"LMMS", "lmms ", "/usr/share/icons/hicolor/16x16/apps/lmms.png" },
+     {"PulseAudio Volume Control", "pavucontrol", "/usr/share/icons/gnome/16x16/apps/multimedia-volume-control.png" },
+     {"Qt V4L2 test Utility", "qv4l2", "/usr/share/icons/hicolor/16x16/apps/qv4l2.png" },
+     {"Qt V4L2 video capture utility", "qvidcap", "/usr/share/icons/hicolor/16x16/apps/qvidcap.png" },
+     {"SimpleScreenRecorder", "simplescreenrecorder --logfile", "/usr/share/icons/hicolor/16x16/apps/simplescreenrecorder.png" },
+     {"VLC media player", "/usr/bin/vlc --started-from-file ", "/usr/share/icons/hicolor/16x16/apps/vlc.png" },
+     {"Volume Icon", "volumeicon", "/usr/share/icons/gnome/16x16/apps/multimedia-volume-control.png" },
+     {"ZynAddSubFX - Alsa", "zynaddsubfx -I alsa -O alsa", "/usr/share/pixmaps/zynaddsubfx.svg" },
+     {"ZynAddSubFX - Jack", "zynaddsubfx -I jack -O jack", "/usr/share/pixmaps/zynaddsubfx.svg" },
+     {"ZynAddSubFX - Jack (multi channel)", "zynaddsubfx -I jack -O jack-multi", "/usr/share/pixmaps/zynaddsubfx.svg" },
+     {"ZynAddSubFX - OSS", "zynaddsubfx -I OSS -O OSS", "/usr/share/pixmaps/zynaddsubfx.svg" },
+     {"mpv Media Player", "mpv --player-operation-mode=pseudo-gui -- ", "/usr/share/icons/hicolor/16x16/apps/mpv.png" },
+ }
+
+ local menuee69799670a33f75d45c57d1d1cd0ab3 = {
+     {"Avahi Zeroconf Browser", "/usr/bin/avahi-discover", "/usr/share/icons/gnome/16x16/devices/network-wired.png" },
+     {"Cool Retro Term", "cool-retro-term", "/usr/share/icons/hicolor/32x32/apps/cool-retro-term.png" },
+     {"Disk Usage Analyzer", "baobab"},
+     {"GParted", "/usr/bin/gparted ", "/usr/share/icons/hicolor/16x16/apps/gparted.png" },
+     {"GSmartControl", "/usr/bin/gsmartcontrol_polkit", "/usr/share/icons/hicolor/16x16/apps/gsmartcontrol.png" },
+     {"Hardware Locality lstopo", "lstopo"},
+     {"Htop", "xterm -e htop", "/usr/share/pixmaps/htop.png" },
+     {"LSHW", "/usr/sbin/gtk-lshw", "///usr/share/lshw/artwork/logo.svg" },
+     {"Manage Printing", "/usr/bin/xdg-open http://localhost:631/", "/usr/share/icons/hicolor/16x16/apps/cups.png" },
+     {"OpenJDK Java 14 Console", "/usr/lib/jvm/java-14-openjdk/bin/jconsole"},
+     {"OpenJDK Java 14 Shell", "xterm -e /usr/lib/jvm/java-14-openjdk/bin/jshell"},
+     {"Tint2", "tint2"},
+     {"UXTerm", "uxterm", "/usr/share/pixmaps/xterm-color_48x48.xpm" },
+     {"Virtual Machine Manager", "virt-manager", "/usr/share/icons/hicolor/16x16/apps/virt-manager.png" },
+     {"WoeUSB", "woeusbgui", "/usr/share/pixmaps/woeusbgui-icon.png" },
+     {"XTerm", "xterm", "/usr/share/pixmaps/xterm-color_48x48.xpm" },
+     {"Xfce Terminal", "xfce4-terminal", "/usr/share/icons/gnome/16x16/apps/utilities-terminal.png" },
+     {"conky", "conky --daemonize --pause=1"},
+     {"nnn", "xterm -e nnn", "/usr/share/icons/hicolor/64x64/apps/nnn.png" },
+ }
+
+xdgmenu = {
+    {"Accessories", menu98edb85b00d9527ad5acebe451b3fae6},
+    {"Education", menude7a22a0c94aa64ba2449e520aa20c99},
+    {"Games", menu251bd8143891238ecedc306508e29017},
+    {"Graphics", menud334dfcea59127bedfcdbe0a3ee7f494},
+    {"Internet", menuc8205c7636e728d448c2774e6a4a944b},
+    {"Office", menudf814135652a5a308fea15bff37ea284},
+    {"Programming", menue6f43c40ab1c07cd29e4e83e4ef6bf85},
+    {"Sound & Video", menu52dd1c847264a75f400961bfb4d1c849},
+    {"System Tools", menuee69799670a33f75d45c57d1d1cd0ab3},
+}
+
